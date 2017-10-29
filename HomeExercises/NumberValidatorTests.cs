537a6a7e64dc5b4871fae7ca9dbacf50f0f4ce98
Alexand Rechkalov <streetonemg10@mail.ru>
2017-10-29 10:16:02 +0500

    Поправки в выполнении домашнего задания

--- HomeExercises/NumberValidatorTests.cs
+++ HomeExercises/NumberValidatorTests.cs
@@ -5,111 +5,110 @@ using NUnit.Framework;
 
 namespace HomeExercises
 {
-    public class NumberValidatorTests
-    {
-        [Test]
-        public void Test()
-        {
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-            Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-            Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+	public class NumberValidatorTests
+	{
+		[Test]
+		public void Test()
+		{
+			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
+			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+			Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
+			Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
 
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-            Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-            Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-            Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-            Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
-        }
+			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
+			Assert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
+			Assert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
+			Assert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
+			Assert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+		}
 
-        [TestCase(3, 2, true, "0", ExpectedResult = true)]
-        [TestCase(3, 2, true, "0.0", ExpectedResult = true)]
-        [TestCase(3, 2, true, "0.00", ExpectedResult = true)]
-        [TestCase(4, 2, true, "0.000", ExpectedResult = false)]
-        [TestCase(3, 2, true, "10", ExpectedResult = true)]
-        [TestCase(3, 2, true, "100", ExpectedResult = true)]
-        [TestCase(3, 2, true, "1000", ExpectedResult = false)]
-        [TestCase(3, 2, false, "+1", ExpectedResult = true)]
-        [TestCase(4, 2, false, "+1.12", ExpectedResult = true)]
-        [TestCase(3, 2, true, null, ExpectedResult = false)]
-        [TestCase(3, 2, true, "-1", ExpectedResult = false)]
-        [TestCase(3, 2, false, "-1", ExpectedResult = true)]
-        [TestCase(3, 2, false, "-1.1", ExpectedResult = true)]
-        [TestCase(3, 2, false, "1", ExpectedResult = true)]
-        [TestCase(3, 2, true, "ab", ExpectedResult = false)]
-        [TestCase(3, 2, true, "+ab", ExpectedResult = false)]
-        [TestCase(3, 2, false, "-ab", ExpectedResult = false)]
-        [TestCase(3, 2, true, "ab.d", ExpectedResult = false)]
-        public static bool ValidateNumber(int precision, int scale, bool onlyPositive, string value)
-        {
-            return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
-        }
+		[TestCase(3, 2, true, "0", ExpectedResult = true, TestName = "Should be valid when value's numbers count before point less than precision")]
+		[TestCase(3, 2, true, "0.00", ExpectedResult = true, TestName = "Should be valid when value's numbers less than precision and numbers after point less than scale")]
+		[TestCase(4, 2, true, "0.000", ExpectedResult = false, TestName = "Should be not valid when value's numbers count less then precision but numbers count after point more than scale")]
+		[TestCase(3, 2, true, "1000", ExpectedResult = false, TestName = "Should be not valid when value's numbers count before point more than precision")]
+		[TestCase(3, 2, false, "+1", ExpectedResult = true, TestName = "Should be valid when value is positive and numberValidator is onlyPositive")]
+		[TestCase(4, 2, false, "+1.12", ExpectedResult = true, TestName = "Should be valid when value is positive and have number after point and numberValidator is onlyPositive")]
+		[TestCase(3, 2, true, null, ExpectedResult = false, TestName = "Should be not valid when value is null")]
+		[TestCase(3, 2, true, "-1", ExpectedResult = false, TestName = "Should be not valid when value is negative, but numberValidator is onliPositive")]
+		[TestCase(3, 2, false, "-1", ExpectedResult = true, TestName = "Should be valid when value is negative and numberValidator is not onliPositive")]
+		[TestCase(3, 2, false, "-1.1", ExpectedResult = true, TestName = "Should be valid when value is negative and have number after point and numberValidator is not onliPositive")]
+		[TestCase(3, 2, false, "1", ExpectedResult = true, TestName = "Should be valid when value is positive but numberValidator is not onliPositive")]
+		[TestCase(3, 2, true, "ab", ExpectedResult = false, TestName = "Should be not valid when value contains letters")]
+		[TestCase(3, 2, true, "+ab", ExpectedResult = false, TestName = "Should be not valid when value start with plus and contains letters")]
+		[TestCase(3, 2, false, "-ab", ExpectedResult = false, TestName = "Should be not valid when value start with minus contains letters")]
+		[TestCase(3, 2, true, "ab.d", ExpectedResult = false, TestName = "Should be not valid when value contains letters and point")]
+		[TestCase(3, 2, true, "1b.d", ExpectedResult = false, TestName = "Should be not valid when value contains letters and number before point")]
+		[TestCase(3, 2, true, "ab.1", ExpectedResult = false, TestName = "Should be not valid when value contains letters and number after point")]
+		[TestCase(3, 2, true, "a1.1", ExpectedResult = false, TestName = "Should be not valid when value contains letters and numbers after and before point")]
+		[TestCase(3, 2, true, "1.1.1", ExpectedResult = false, TestName = "Should be not valid when value contains more then one point")]
+		public static bool ValidateNumber(int precision, int scale, bool onlyPositive, string value)
+		{
+			return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
+		}
 
-        [TestCase(-1, 2, true, true)]
-        [TestCase(1, -1, true, true)]
-        [TestCase(1, 2, true, true)]
-        [TestCase(2, 1, true, false)]
-        [TestCase(0, 1, true, true)]
-        [TestCase(1, 0, true, false)]
-        public void Throw(int precision, int scale, bool onlyPositive, bool isShouldThrow)
-        {
-            Action act = () => new NumberValidator(precision, scale, onlyPositive);
-            if (isShouldThrow) act.ShouldThrow<ArgumentException>();
-            else act.ShouldNotThrow();
-        }
+		[TestCase(-1, 2, true, TestName = "When precision is negative")]
+		[TestCase(1, -1, true, TestName = "When scale is negative")]
+		[TestCase(1, 2, true, TestName = "When scale is more than precision")]
+		[TestCase(0, 1, true, TestName = "When precision is Zero")]
+		public void NumberValidator_ShouldThrow(int precision, int scale, bool onlyPositive)
+		{
+			Action act = () => new NumberValidator(precision, scale, onlyPositive);
 
-    }
+			act.ShouldThrow<ArgumentException>();
+		}
 
-    public class NumberValidator
-    {
-        private readonly Regex numberRegex;
-        private readonly bool onlyPositive;
-        private readonly int precision;
-        private readonly int scale;
+	}
 
-        public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
-        {
-            this.precision = precision;
-            this.scale = scale;
-            this.onlyPositive = onlyPositive;
-            if (precision <= 0)
-                throw new ArgumentException("precision must be a positive number");
-            if (scale < 0 || scale >= precision)
-                throw new ArgumentException("precision must be a non-negative number less or equal than precision");
-            numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
-        }
+	public class NumberValidator
+	{
+		private readonly Regex numberRegex;
+		private readonly bool onlyPositive;
+		private readonly int precision;
+		private readonly int scale;
 
-        public bool IsValidNumber(string value)
-        {
-            // Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
-            // описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
-            // Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
-            // целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
-            // Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
+		public NumberValidator(int precision, int scale = 0, bool onlyPositive = false)
+		{
+			this.precision = precision;
+			this.scale = scale;
+			this.onlyPositive = onlyPositive;
+			if (precision <= 0)
+				throw new ArgumentException("precision must be a positive number");
+			if (scale < 0 || scale >= precision)
+				throw new ArgumentException("precision must be a non-negative number less or equal than precision");
+			numberRegex = new Regex(@"^([+-]?)(\d+)([.,](\d+))?$", RegexOptions.IgnoreCase);
+		}
 
-            if (string.IsNullOrEmpty(value))
-                return false;
+		public bool IsValidNumber(string value)
+		{
+			// Проверяем соответствие входного значения формату N(m,k), в соответствии с правилом, 
+			// описанным в Формате описи документов, направляемых в налоговый орган в электронном виде по телекоммуникационным каналам связи:
+			// Формат числового значения указывается в виде N(m.к), где m – максимальное количество знаков в числе, включая знак (для отрицательного числа), 
+			// целую и дробную часть числа без разделяющей десятичной точки, k – максимальное число знаков дробной части числа. 
+			// Если число знаков дробной части числа равно 0 (т.е. число целое), то формат числового значения имеет вид N(m).
 
-            var match = numberRegex.Match(value);
-            if (!match.Success)
-                return false;
+			if (string.IsNullOrEmpty(value))
+				return false;
 
-            // Знак и целая часть
-            var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
-            // Дробная часть
-            var fracPart = match.Groups[4].Value.Length;
+			var match = numberRegex.Match(value);
+			if (!match.Success)
+				return false;
 
-            if (intPart + fracPart > precision || fracPart > scale)
-                return false;
+			// Знак и целая часть
+			var intPart = match.Groups[1].Value.Length + match.Groups[2].Value.Length;
+			// Дробная часть
+			var fracPart = match.Groups[4].Value.Length;
 
-            if (onlyPositive && match.Groups[1].Value == "-")
-                return false;
-            return true;
-        }
-    }
+			if (intPart + fracPart > precision || fracPart > scale)
+				return false;
+
+			if (onlyPositive && match.Groups[1].Value == "-")
+				return false;
+			return true;
+		}
+	}
 }
--- HomeExercises/ObjectComparison.cs
+++ HomeExercises/ObjectComparison.cs
@@ -1,216 +1,129 @@
 ﻿using System;
 using System.CodeDom;
 using System.Linq.Expressions;
+using System.Threading;
 using FluentAssertions;
 using FluentAssertions.Equivalency;
 using NUnit.Framework;
 
 namespace HomeExercises
 {
-    public class ObjectComparison
-    {
-        [Test]
-        [Description("Проверка текущего царя")]
-        [Category("ToRefactor")]
-        public void CheckCurrentTsar()
-        {
+	public class ObjectComparison
+	{
+		[Test]
+		[Description("Проверка текущего царя")]
+		[Category("ToRefactor")]
+		public void CheckCurrentTsar()
+		{
 
-            // Перепишите код на использование Fluent Assertions.
-            actualTsar.Name.Should().Be(expectedTsar.Name);
-            actualTsar.Age.Should().Be(expectedTsar.Age);
-            actualTsar.Height.Should().Be(expectedTsar.Height);
-            actualTsar.Weight.Should().Be(expectedTsar.Weight);
+			// Перепишите код на использование Fluent Assertions.
+			actualTsar.Name.Should().Be(expectedTsar.Name);
+			actualTsar.Age.Should().Be(expectedTsar.Age);
+			actualTsar.Height.Should().Be(expectedTsar.Height);
+			actualTsar.Weight.Should().Be(expectedTsar.Weight);
 
-            actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
-            actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
-            actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
-            actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
+			actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
+			actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
+			actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
+			actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
 
-        }
+		}
 
-        [Test]
-        [Description("Альтернативное решение. Какие у него недостатки?")]
-        public void CheckCurrentTsar_WithCustomEquality()
-        {
+		[Test]
+		[Description("Альтернативное решение. Какие у него недостатки?")]
+		public void CheckCurrentTsar_WithCustomEquality()
+		{
 
-            // Какие недостатки у такого подхода? 
-            // Если тест упадет, мы не узнаем из результата какие свойства не совпадают
-            // Но в отличает от CheckCurrentTsar тут учитывается связная структура класса Person
-            // Если у родителя царя добавить родителя (создать новые экземпляры класса Person с одинаковыми параметрами в ожидаемом и актуальном результатах), 
-            // то тест CheckCurrentTsar упадет из за id
-            // В данном случае метод AreEqual рекурсивно проверяет всех родителей
-            Assert.True(AreEqual(actualTsar, expectedTsar));
-        }
+			// Какие недостатки у такого подхода? 
+			// Если тест упадет, мы не узнаем из результата какие свойства не совпадают
+			// Но в отличает от CheckCurrentTsar тут учитывается связная структура класса Person
+			// Если у родителя царя добавить родителя (создать новые экземпляры класса Person с одинаковыми параметрами в ожидаемом и актуальном результатах), 
+			// то тест CheckCurrentTsar упадет из за id
+			// В данном случае метод AreEqual рекурсивно проверяет всех родителей
+			Assert.True(AreEqual(actualTsar, expectedTsar));
+		}
 
-        private bool AreEqual(Person actual, Person expected)
-        {
-            if (actual == expected) return true;
-            if (actual == null || expected == null) return false;
-            return
-            actual.Name == expected.Name
-            && actual.Age == expected.Age
-            && actual.Height == expected.Height
-            && actual.Weight == expected.Weight
-            && AreEqual(actual.Parent, expected.Parent);
-        }
+		private bool AreEqual(Person actual, Person expected)
+		{
+			if (actual == expected) return true;
+			if (actual == null || expected == null) return false;
+			return
+			actual.Name == expected.Name
+			&& actual.Age == expected.Age
+			&& actual.Height == expected.Height
+			&& actual.Weight == expected.Weight
+			&& AreEqual(actual.Parent, expected.Parent);
+		}
 
-        /*
-         * Вариант 1
-         * Разделение каждого свойства на отдельные методы позволяет видеть в результате свойство, на котором упал тест
-         * Например если изменить свойство Age, то тест с именем SameAges упадет и будет видно различие между ожидаемым и полученным результатом
-         * Если у объекта Person изменится конструктор, то достаточно исправить создание переменной expectedTsar в SetUp методе
-         * Если у объекта Person увеличится количество свойств, то достаточно создать 1 тестовый метод, покрывающий это свойство
-         * Писать для каждого свойства тестирующий метод слишком жирно (и тривиально), поэтому этот подход не годится.
+		/*
+         * С помощью метода ShouldBeEquivalentTo можно сравнить два объекта по всем свойствам исключая лишние
+		 * Так как класс Person имеет односвязную структуру, то необходимо рекурсивно проверять родителей
+		 * Поэтому нужно разрешить бесконечную рекурсию с помощью AllowingInfiniteRecursion
+		 * Если исключить из сравнения только Id, то любые два родителя будут не эквивалентны, так как их Id разные
+		 * Исключить сравнение Id у всех Parent можно двумя способами
+		 * 1. Можно изменить сравнение Id у всех Parent так, чтобы любые два Id были одинаковыми
+		 * Я сделал так, чтобы два разных Id сравнивались по типу данных, что в данном случае всегда будут совпадать
+		 * 2. Можно исключить все свойства заканчивающиеся на .Id
+		 * В данном случае такие свойства как Parent.Id, Parent.Parent.Id и так далее будут удовлетворять данному условию, и исключаться из сравнения
          */
-        private Person expectedTsar;
-        private Person actualTsar;
+		private Person expectedTsar;
+		private Person actualTsar;
 
-        [SetUp]
-        public void SetUp()
-        {
-            actualTsar = TsarRegistry.GetCurrentTsar();
-            expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-                new Person("Vasili III of Russia", 28, 170, 60, null));
-        }
+		[SetUp]
+		public void SetUp()
+		{
+			actualTsar = TsarRegistry.GetCurrentTsar();
+			expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
 
-        [Test]
-        public void SameNames()
-        {
-            actualTsar.Name.Should().Be(expectedTsar.Name);
-        }
+		[Test]
+		public void CompareTsars()
+		{
+			expectedTsar.ShouldBeEquivalentTo(actualTsar, options => options
+				.AllowingInfiniteRecursion()
+				.Excluding(thar => thar.Id)
+				.Using<Person>(info => info.Subject.Id.Should().BeOfType(info.Expectation.Id.GetType()))
+				.WhenTypeIs<Person>());
+		}
 
-        [Test]
-        public void SameAges()
-        {
-            actualTsar.Age.Should().Be(expectedTsar.Age);
-        }
+		[Test]
+		public void CompareTsars2()
+		{
+			expectedTsar.ShouldBeEquivalentTo(actualTsar, options => options
+				.AllowingInfiniteRecursion()
+				.Excluding(thar => thar.Id)
+				.Excluding(thar => thar.SelectedMemberPath.EndsWith(".Id")));
+		}
 
-        [Test]
-        public void SameHeights()
-        {
-            actualTsar.Height.Should().Be(expectedTsar.Height);
-        }
+	}
 
-        [Test]
-        public void SameWeights()
-        {
-            actualTsar.Weight.Should().Be(expectedTsar.Weight);
-        }
+	public class TsarRegistry
+	{
+		public static Person GetCurrentTsar()
+		{
+			return new Person(
+				"Ivan IV The Terrible", 54, 170, 70,
+				new Person("Vasili III of Russia", 28, 170, 60, null));
+		}
+	}
 
-        [Test]
-        public void SameParentsNames()
-        {
-            actualTsar.Parent.Name.Should().Be(expectedTsar.Parent.Name);
-        }
+	public class Person
+	{
+		public static int IdCounter = 0;
+		public int Age, Height, Weight;
+		public string Name;
+		public Person Parent;
+		public int Id;
 
-        [Test]
-        public void SameParentsAges()
-        {
-            actualTsar.Parent.Age.Should().Be(expectedTsar.Parent.Age);
-        }
-
-        [Test]
-        public void SameParentsHeights()
-        {
-            actualTsar.Parent.Height.Should().Be(expectedTsar.Parent.Height);
-        }
-
-        [Test]
-        public void SameParentsParents()
-        {
-            actualTsar.Parent.Parent.Should().Be(expectedTsar.Parent.Parent);
-        }
-
-        /*
-         * Вариант 2
-         * Способ включает в себя все преимущества первого варианта, но при этом имеет всего один метод
-         * Чтобы указать какие свойства нужно сравнивать, необходимо добавить его в опции сравнения с помощью метода Including
-         * Таким образом, при добавлении нового свойства, нужно добавить одну строчку в метод, чтобы новое свойство тоже сравнивалось
-         * Если одно или несколько свойств будут различаться, то в результате будут показаны все различия
-         */
-        [Test]
-        public void TestWithIncluding()
-        {
-            expectedTsar.ShouldBeEquivalentTo(actualTsar, options => options
-                .Including(thar => thar.Name)
-                .Including(thar => thar.Age)
-                .Including(thar => thar.Height)
-                .Including(thar => thar.Weight)
-                .Including(thar => thar.Parent.Name)
-                .Including(thar => thar.Parent.Age)
-                .Including(thar => thar.Parent.Height)
-                .Including(thar => thar.Parent.Parent));
-        }
-
-        /*
-         * Вариант 3
-         * Вариант аналогичен второму варианту, только теперь мы не включаем свойства в сравнение, а наоборот исключаем их
-         * В рассматриваемом объекте не сравниваемых свойств оказалось меньше, чем сравниваемых
-         * Поэтому можно сравнить объекты по всем свойствам, исключая лишние
-         * В этом случае, при добавлении новых свойств, необходимо расширять тест только в том случае, если новое свойство не нужно рассматривать
-         */
-        [Test]
-        public void WestWithExcluding()
-        {
-            expectedTsar.ShouldBeEquivalentTo(actualTsar, options => options
-                .Excluding(thar => thar.Id)
-                .Excluding(thar => thar.Parent.Id));
-        }
-
-        /* Вариант 4
-         * Во всех предыдущих вариантах не было рассмотрено случая, когда у родителя тоже есть родитель (не null)
-         * В такой ситуации все тесты, сравнивающие thar.Parent.Parent упадут (за исключением CheckCurrentTsar_WithCustomEquality)
-         * В данном варианте мы параллельно идем по родителям ожидаемого и актуального царя и сравниваем их
-         * С помощью сообщения можно узнать на каком родителе(в случае самого царя - 0) произошла ошибка
-         * При необходимости можно написать алгоритм, выводящий полную последовательность сравниваемых людей 
-         * Я считаю данный вариант наилучшим, так как он сравнивает всю последовательность царей, исключая лишние свойства, так же как в 3ем варианте
-         * При падении теста он выведет свойство, из-за которого упал тест, а так же родителя, у которого это свойство рассматривалось
-         */
-        [Test]
-        public void TestWithLoop()
-        {
-            var expect = expectedTsar;
-            var actual = actualTsar;
-            var count = 0;
-            while (expect != null && actual != null)
-            {
-                expect.ShouldBeEquivalentTo(actual, options => options
-                    .Excluding(thar => thar.Id)
-                    .Excluding(thar => thar.Parent), "Порядок родителя {0}", count++);
-                expect = expect.Parent;
-                actual = actual.Parent;
-            }
-            expect.ShouldBeEquivalentTo(actual);
-        }
-
-    }
-
-    public class TsarRegistry
-    {
-        public static Person GetCurrentTsar()
-        {
-            return new Person(
-                "Ivan IV The Terrible", 54, 170, 70,
-                new Person("Vasili III of Russia", 28, 170, 60, null));
-        }
-    }
-
-    public class Person
-    {
-        public static int IdCounter = 0;
-        public int Age, Height, Weight;
-        public string Name;
-        public Person Parent;
-        public int Id;
-
-        public Person(string name, int age, int height, int weight, Person parent)
-        {
-            Id = IdCounter++;
-            Name = name;
-            Age = age;
-            Height = height;
-            Weight = weight;
-            Parent = parent;
-        }
-    }
+		public Person(string name, int age, int height, int weight, Person parent)
+		{
+			Id = IdCounter++;
+			Name = name;
+			Age = age;
+			Height = height;
+			Weight = weight;
+			Parent = parent;
+		}
+	}
 }
